0518452812556d95ba0394696f3a4ee5b99fda7f
lby <852811848@qq.com>
2016-12-05 10:23:52 +0800

    手动导入SnapKit框架

--- BYMusic/BYMusic/BYHomeVC.swift
+++ BYMusic/BYMusic/BYHomeVC.swift
@@ -8,31 +8,64 @@
 
 import UIKit
 import SnapKit
+
+//宏
 let SCREEN_WIDTH = UIScreen.main.bounds.width
 let SCREEN_HEIGHT = UIScreen.main.bounds.height
 
 
-class BYHomeVC: UIViewController {
+class BYHomeVC: UINavigationController
+{
 
-     public func RGB(r:CGFloat,g:CGFloat,b:CGFloat)->UIColor{
-        
+     public func RGB(r:CGFloat,g:CGFloat,b:CGFloat)->UIColor
+     {
         return UIColor(red: r/255.0, green: g/255.0, blue: b/255.0, alpha: 1)
-        
     }
     
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.view.backgroundColor = self.RGB(r: 60,g: 179,b: 113)
         
-        let topView = UIView(frame: CGRect(x: 0,y: 0,width: SCREEN_WIDTH,height: 66))
-        topView.backgroundColor = UIColor.init(red: 46/255.0, green: 139/255.0, blue: 89/255.0, alpha: 1.0)
-        let topBtn = UILabel(frame: CGRect(x: 50, y: 5, width: 30, height: 20))
-        topView.addSubview(topBtn)
-        self.view.addSubview(topView)
+        //视图背景色
+        self.view.backgroundColor = self.RGB(r: 60,g: 179,b: 113)
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "img_01"), style: .plain, target: self, action: #selector(showPerFormation))
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "img_02"), style: .plain, target: self, action: #selector(search))
+        
+        //头部视图
+//        let topView = UIView(frame: CGRect(x: 0,y: 0,width: SCREEN_WIDTH,height: 66))
+//        let topView = UIView()
+//        topView.snp.makeConstraints { (make) in
+        
+//        }
+//        topView.backgroundColor = self.RGB(r: 46, g: 139, b: 255)
+        
+//        let topLab = UILabel(frame: CGRect(x: 50, y: 5, width: 30, height: 20))
+        let topLab = UILabel()
+//        topView.addSubview(topLab)
+        //约束
+        topLab.snp.makeConstraints { (make) in
+            make.left.right.equalTo(self.view)
+            make.top.equalTo(self.view.snp.top)
+            make.height.equalTo(40)
+        }
+        
+//        self.view.addSubview(topView)
+    }
+    
+    //个人信息展示
+    func showPerFormation() -> Void
+    {
+        self.navigationController?.pushViewController(UIViewController() , animated: true)
     }
 
-    override func didReceiveMemoryWarning() {
+    //查询
+    func search() -> Void
+    {
+        self.navigationController?.pushViewController(UIViewController() , animated: true)
+    }
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
